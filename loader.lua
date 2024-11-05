07d9490864a86e0968efd083b004b39fd2efdb90
Icon <uhalozh@gmail.com>
2024-11-05 18:38:38 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,12 +1,18 @@
+-- Define game-specific loadstrings in a table
 local gameidloadstrings = {
-    [5130598377] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/Halohz/IconicHub/refs/heads/main/gamepage.lua'))()",
-    [6846458508] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/Halohz/IconicHub/refs/heads/main/maingame.lua'))()"
+    [5130598377] = "https://raw.githubusercontent.com/Halohz/IconicHub/refs/heads/main/gamepage.lua",
+    [6846458508] = "https://raw.githubusercontent.com/Halohz/IconicHub/refs/heads/main/maingame.lua"
 }
 
+-- Get the current game's ID
 local currentGameId = game.PlaceId
 
+-- Check if there's a matching URL for the current game
 if gameidloadstrings[currentGameId] then
-    gameidloadstrings(game:HttpGet(gameidloadstrings[currentGameId]))()
+    -- Fetch the script content and execute it
+    local scriptContent = game:HttpGet(gameidloadstrings[currentGameId])
+    loadstring(scriptContent)()
 else
+    -- If the game ID does not match, print a message
     print("You're not in a supported AUT game :/")
 end
